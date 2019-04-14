9dbc29995d27cea183afd18133de77d40d512337
Shahid Shah <shahid@shah.org>
2019-04-13 23:08:12 -0400

    Refactored Collection to use Cache

--- score_test.go
+++ score_test.go
@@ -4,6 +4,7 @@ import (
 	"net/url"
 	"testing"
 
+	"github.com/lectio/score/container"
 	"github.com/stretchr/testify/suite"
 )
 
@@ -40,10 +41,10 @@ func (suite *ScoreSuite) TestCollection() {
 	handler := func(index int) (*url.URL, error) {
 		return urls[index], nil
 	}
-	iterator := func() (startIndex int, endIndex int, keys Keys, retrievalFn TargetsIteratorRetrievalFn) {
+	iterator := func() (startIndex int, endIndex int, keys Keys, retrievalFn container.TargetsIteratorRetrievalFn) {
 		return 0, len(urls) - 1, suite.keys, handler
 	}
-	sc := MakeCollection(iterator, nil, true)
+	sc := container.MakeCollection(iterator, nil, true)
 	suite.NotNil(sc, "Scores collection should not be Nil")
 }
 
